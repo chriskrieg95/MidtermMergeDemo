c92acbecf6a73b59cd9c1b5177f8834efbb30f0f
graffadamr <graff.adam.r@gmail.com>
2019-10-31 10:47:47 -0400

    added line

--- src/co/grandcircus/MainApp.java
+++ src/co/grandcircus/MainApp.java
@@ -5,6 +5,7 @@ public class MainApp {
 	public static void main(String[] args) {
 		 System.out.println("Hello World!");
 		 System.out.println("Hello Adam!");
+		 System.out.println("Hello Chris!");
 
 	}
 
